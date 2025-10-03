8ee781786ca4dcf40993416e42ba08b9f9c42fdc
Gaan_Dmitry <gaandima55@yandex.ru>
2025-10-03 21:49:52 +0600

    text center

--- footer.php
+++ footer.php
@@ -19,12 +19,12 @@
 
 <!-- Модалка согласия на Cookies -->
 <div class="modal fade" id="cookieModal" tabindex="-1" aria-labelledby="cookieModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered">
+  <div class="modal-dialog modal-dialog-centered d-flex align-items-center">
     <div class="modal-content">
       <div class="modal-header bg-primary text-white">
-        <h5 class="modal-title" id="cookieModalLabel">Мы используем cookies</h5>
+        <h5 class="modal-title text-center" id="cookieModalLabel">Мы используем cookies</h5>
       </div>
-      <div class="modal-body">
+      <div class="modal-body text-center">
         <p>
           На сайте используются cookies для улучшения работы и анализа трафика. 
           Продолжая пользоваться сайтом, вы соглашаетесь с нашей 
